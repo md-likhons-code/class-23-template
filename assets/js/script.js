b3cc9b46c56ce9988277e1e2efc154aa17780845
MD Likhon <mdlikhon0925@gmail.com>
2025-04-10 14:32:21 +0600

    Updated files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 jQuery(document).ready(function() {
     $(".popup").magnificPopup({
         disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
+        type: 'iframe',
+        removalDelay: 160,
+        preloader: false,
         fixedContentPos: false,
 
         iframe: {
@@ -28,5 +27,13 @@ jQuery(document).ready(function() {
 
     $(".menu-btn").click(function() {
       $(".mobile-menu").slideToggle("slow");
+    });
+
+    $(window).scroll(function(){
+      if($(window).scrollTop() > 100){
+          $(".header-top").addClass("bg-[#f7f7f7] shadow-md");
+      }else{
+        $(".header-top").removeClass("bg-[#f7f7f7] shadow-md");
+      }
     });
 });
